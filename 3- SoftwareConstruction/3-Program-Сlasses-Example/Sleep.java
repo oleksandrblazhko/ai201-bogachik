4cb566b614e887d27c81f9dc98fd0033b9cfb355
kovasnik <79566284+kovasnik@users.noreply.github.com>
2022-11-02 14:43:26 +0200

    Update Sleep.java

--- 3- SoftwareConstruction/3-Program-Сlasses-Example/Sleep.java
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example/Sleep.java
@@ -8,28 +8,31 @@ public class Sleep {
     private int hours_sleep; // кількість годин сну
 
 
-    public String getData() {
-        return data;
-    }
-
     public User getHuman() {
         return human;
     }
 
-    public void setData(String data) {
-        this.data = data;
+    public int recordSleep(int sleep_quality, int hours_sleep, String data){// введення параметрів сну
+        this.hours_sleep = hours_sleep; // введення у базу нового значення годин сну
+        this.sleep_quality = sleep_quality; // введення у базу нового значення якості сну
+        this.data = data; // введення у базу нової дати
+        return 0;// повернення значення 0 після виповнення методу
     }
 
-    public int recordSleep(int sleep_quality, int hours_sleep){
-        this.hours_sleep = hours_sleep;
-        this.sleep_quality = sleep_quality;
-        return 0;
+    public int[] viewSleep(){ // виведення покаників сну
+        int A[] = new int[2]; // створення масиву для виведення значень
+        A[0] = sleep_quality; // занесення у масив значень якості сну
+        A[1] = hours_sleep; // занесення у масив кількість годин сну
+        return A; // повернення значнь масиву
     }
 
-    public int[] viewSleep(){
-        int A[] = new int[2];
-        A[0] = sleep_quality;
-        A[1] = hours_sleep;
-        return A;
+    @Override
+    public String toString() {
+        return "Sleep{" +
+                "sleep_quality=" + sleep_quality +
+                ", hours_sleep=" + hours_sleep +
+                ", data=" + data +
+                '}';
     }
 }
+
