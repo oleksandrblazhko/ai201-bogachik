757e14a5e382bee6548372225617758e9c8f878a
kovasnik <79566284+kovasnik@users.noreply.github.com>
2022-11-02 14:45:13 +0200

    Update Health.java

--- 3- SoftwareConstruction/3-Program-Сlasses-Example/Health.java
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example/Health.java
@@ -1,13 +1,15 @@
 package com.company.sleep;
 
 public class Health {
-    private int[] psychological_state; // псигологічний стан людини
+    private int psychological_state; // псигологічний стан людини
     private User human_health; //користувач
-    private int[] emotional_condition; // емоційний стан людини
-    private String[] data; // дата заповнення
+    private int emotional_condition; // емоційний стан людини
+    private String data; // дата заповнення
     private Сourses course; // поточний курс на якому перебуває людина
 
-    public int[] getPsychological_state() {
+    
+    
+    public int getPsychological_state() {
         return psychological_state;
     }
 
@@ -15,11 +17,11 @@ public class Health {
         return human_health;
     }
 
-    public int[] getEmotional_condition() {
+    public int getEmotional_condition() {
         return emotional_condition;
     }
 
-    public String[] getData() {
+    public String getData() {
         return data;
     }
 
@@ -27,7 +29,7 @@ public class Health {
         return course;
     }
 
-    public void setPsychological_state(int[] psychological_state) {
+    public void setPsychological_state(int psychological_state) {
         this.psychological_state = psychological_state;
     }
 
@@ -35,11 +37,11 @@ public class Health {
         this.human_health = human_health;
     }
 
-    public void setEmotional_condition(int[] emotional_condition) {
+    public void setEmotional_condition(int emotional_condition) {
         this.emotional_condition = emotional_condition;
     }
 
-    public void setData(String[] data) {
+    public void setData(String data) {
         this.data = data;
     }
 
